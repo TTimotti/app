ac92a90e1870a347705f7d827b462257b5d18b30
TTimotti <ttimotti@kakao.com>
2023-11-22 15:04:57 +0900

    날씨 API 설정

--- js/weather.js
+++ js/weather.js
@@ -2,14 +2,20 @@ const API_KEY = "402edfc1b82430cccab38d5f2ebd7186";
 const onGeoSuccess = (position) => {
     const lat = position.coords.latitude;
     const lon = position.coords.longitude;
-    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;    
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric&lang=kr`;    
     fetch(url)
     .then(response => response.json())
     .then(data => {
         const weather = document.querySelector("#weather span:first-child");
-        const city = document.querySelector("#weather span:last-child");
+        const city = document.querySelector("#weather span:nth-child(3)");
+        const temp = document.querySelector("#weather span:nth-child(5)");
+        const humid = document.querySelector("#weather span:nth-child(6)");
+        const weatherId = data.weather[0].id;
+        const description = weatherDescKo.find(item => Object.keys(item)[0] === `${weatherId}`)?.[weatherId];
         weather.innerText = data.name;
-        city.innerText = `${data.weather[0].main}, ${data.main.temp}`;    
+        city.innerText = description;
+        temp.innerText = Math.round(data.main.temp);
+        humid.innerText = Math.round(data.main.humidity);
     });    
 }
 const onGeoError = () => {
